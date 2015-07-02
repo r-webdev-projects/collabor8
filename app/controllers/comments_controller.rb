8abd540fec077c180d9815f8f6670767f245b081
Josh Stricklin <jostricklin@gmail.com>
2015-07-02 00:58:49 -0500

    Added ability to delete comments. If idea is deleted, so are its comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
 		redirect_to idea_path(@idea)
 	end
 
+	def destroy
+		@idea = Idea.find(params[:idea_id])
+		@comment = @idea.comments.find(params[:id])
+		@comment.destroy
+		redirect_to idea_path(@idea)
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,3 @@
 class Idea < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 end

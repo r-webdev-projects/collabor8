d274cb9cf7646b8392816703fed0dba262c5e4b0
Josh Stricklin <jostricklin@gmail.com>
2015-07-02 00:54:48 -0500

    Created comment form partial, added to ideas view.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 	def create
 		@idea = Idea.find(params[:idea_id])
 		@comment = @idea.comments.create(comment_params)
-		redirect_to_idea_path(@idea)
+		redirect_to idea_path(@idea)
 	end
 
 	private

a5865f66c96561db4ba617bbdf79790eb93617f0
Josh Stricklin <jostricklin@gmail.com>
2015-07-02 00:18:02 -0500

    Migrated comments into db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627013339) do
+ActiveRecord::Schema.define(version: 20150702045841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["idea_id"], name: "index_comments_on_idea_id", using: :btree
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 20150627013339) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "comments", "ideas"
 end

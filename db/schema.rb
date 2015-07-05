4cdbc2bd296721a5b93f3f1c1436c6a338abb9a9
Vishal Arora <vishal@vishalarora.com>
2015-07-05 12:30:33 -0400

    Add location & short bio to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629195532) do
+ActiveRecord::Schema.define(version: 20150705162842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["idea_id"], name: "index_comments_on_idea_id", using: :btree
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
@@ -39,9 +49,12 @@ ActiveRecord::Schema.define(version: 20150629195532) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "location"
+    t.text     "short_bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "ideas"
 end

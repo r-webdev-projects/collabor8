09ed7691c6c2c456f404218f88ba55312447f791
Josh Stricklin <jostricklin@gmail.com>
2015-07-13 13:19:55 -0500

    removed line referencing nonexisttent user_id in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,6 @@ class CommentsController < ApplicationController
 	def create
 		@idea = Idea.find(params[:idea_id])
 		@comment = @idea.comments.create(comment_params)
-
-		# associate comment with user on creation
-		@comment.user = current_user
-
 		redirect_to idea_path(@idea)
 	end
 

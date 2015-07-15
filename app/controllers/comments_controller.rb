2dbb2049a5962be05bf3ddd78c131fafa812e037
Josh Stricklin <jostricklin@gmail.com>
2015-07-15 18:49:02 -0500

    reverted comments, added test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 	def create
 		@idea = Idea.find(params[:idea_id])
 		@comment = @idea.comments.create(comment_params)
-		@comment.user_id = current_user.id
+		# @comment.user_id = current_user.id
 		redirect_to idea_path(@idea)
 	end
 

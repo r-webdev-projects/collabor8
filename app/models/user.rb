1760aa74c866c95bcf51a9fb71b1be56bcbc0321
Vishal Arora <vishal@vishalarora.com>
2015-07-15 21:40:49 -0400

    Make current_user accessible across entire app

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          # :confirmable, :lockable, :timeoutable and :omniauthable
 
+  cattr_accessor :current_user
+
 end

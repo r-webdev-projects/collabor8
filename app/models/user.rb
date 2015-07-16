d2ad157616ce4be764cd648da1661588868d9e1e
Vishal Arora <vishal@vishalarora.com>
2015-07-15 20:52:31 -0400

    Clean up User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,12 @@
-class User < ActiveRecord::Base 
-  
-  validates :first_name, presence: true, length: { in: 3..24 } #totally arbitrary upper limit
+class User < ActiveRecord::Base
+
   has_many :ideas
-  has_many :comments, foreign_key: :author_id # once comments get implemented
-  
-  
-  #devise stuff below
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  
-  #add :confirmable if you want to confirm email
+  has_many :comments, foreign_key: :author_id
+
+  validates :first_name, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+         # :confirmable, :lockable, :timeoutable and :omniauthable
+
 end

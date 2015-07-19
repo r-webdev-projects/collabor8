53372b1b9efa3f481dc1fda1d60bf33edad12fa7
Josh Stricklin <jostricklin@gmail.com>
2015-07-19 13:52:33 -0500

    removed commenter text field from form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,6 @@ class CommentsController < ApplicationController
 	def create
 		@idea = Idea.find(params[:idea_id])
 		@comment = @idea.comments.create(comment_params)
-		# @comment.user_id = current_user.id
 		redirect_to idea_path(@idea)
 	end
 
